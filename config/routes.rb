627ee541c3d13a5aeefb5141d3c3b5f7ae184139
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-23 04:40:31 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  resources :artists, :except => [:create, :update, :show, :destroy]
   resources :songs
 
   get '/artists/new', to: 'artists#show', id: "new"
