4537e9f1a61c11a9f7129e561325873f1d5752f7
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-23 03:41:44 +0000

    automatically backed up by learn

--- app/admin/songs.rb
+++ app/admin/songs.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register Author do
+ActiveAdmin.register Song do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
